8261e6f844a3fc8adeb106b643b0361f90c5539b
Rajkumar Natarajan <rajkumar.natarajan@synacor.com>
2017-12-13 11:46:50 -0500

    Try Autowiring

--- aerospike-benchmark-demo/src/jmh/java/com/aerospike/benchmark/AerospikeCreateBenchMarkDemo.java
+++ aerospike-benchmark-demo/src/jmh/java/com/aerospike/benchmark/AerospikeCreateBenchMarkDemo.java
@@ -1,5 +1,6 @@
 package com.aerospike.benchmark;
 
+import com.aerospike.client.query.IndexType;
 import com.demo.aerospike.config.AerospikeConfig;
 import com.demo.aerospike.entity.Person;
 import com.demo.aerospike.repositories.PersonRepository;
@@ -7,30 +8,83 @@ import org.openjdk.jmh.annotations.*;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+/*@SpringBootApplication
+@EnableAutoConfiguration
+@EntityScan("com.demo.aerospike.entity")
+@ComponentScan(basePackages = {"com.demo.aerospike", "com.demo.aerospike"})
+@EnableJpaRepositories("com.demo.aerospike.repositories")*/
+
 
 public class AerospikeCreateBenchMarkDemo {
 
     @State(Scope.Benchmark)
     public static class AerospikeCreateBenchmarkPlan {
 
+        @Autowired
+        private ApplicationContext context;
+
+        @Autowired
+        private PersonRepository personRepository;
+
+        public PersonRepository getPersonRepository() {
+            try {
+                String args = "";
+                if(context == null) {
+                    context = SpringApplication.run(AerospikeConfig.class, args );
+                }
+                personRepository = context.getBean(PersonRepository.class);
+                if(personRepository != null)
+                    System.out.println(personRepository + " is not null");
+                else
+                    System.out.println(personRepository + "is null");
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
+            return personRepository;
+        }
+
         @Param({ "10" })
         public int iterations;
 
         public List<Person> persons = new ArrayList<>();
 
-        //public PersonRepository repository;
-
         Random rand = new Random();
 
-        @Setup(Level.Invocation)
+        @Setup(Level.Trial)
         public void setUp() {
+            try {
+                String args = "";
+                if(context == null) {
+                    context = SpringApplication.run(AerospikeConfig.class, args );
+                }
+                personRepository = context.getBean(PersonRepository.class);
+                if(personRepository != null)
+                System.out.println(personRepository + " is not null");
+                else
+                    System.out.println(personRepository + "is null");
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
             for(int i=0; i < iterations; i++) {
                 persons.add(generatePerson());
             }
@@ -49,9 +103,17 @@ public class AerospikeCreateBenchMarkDemo {
     @BenchmarkMode(Mode.AverageTime)
     @Warmup(iterations = 5)
     public void createPerson(AerospikeCreateBenchmarkPlan plan) {
-        ApplicationContext ctx = new AnnotationConfigApplicationContext(AerospikeConfig.class);
-        PersonRepository repository = ctx.getBean(PersonRepository.class);
-        plan.persons.forEach(p -> repository.save(p));
+        PersonRepository personRepository;
+
+        if(plan.getPersonRepository() != null) {
+            personRepository = plan.getPersonRepository();
+        } else {
+            ApplicationContext context = SpringApplication.run(AerospikeConfig.class, "" );
+            personRepository = context.getBean(PersonRepository.class);
+        }
+
+
+        plan.persons.forEach(p -> plan.getPersonRepository().save(p));
     }
 
 }
